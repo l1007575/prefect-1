c2f0ef22fc34867c541e23dc8089b88fb5cc001a
l1007575 <alexiscarbonero89@gmail.com>
2025-08-03 14:39:33 -0300

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,26 +1,9 @@
-#!/usr/bin/env bash
-# This is the default entrypoint for the official Prefect Docker image
-
+#!/bin/sh
 set -e
 
-if [ -f ~/.bashrc ]; then
-  . ~/.bashrc
-fi
-
-if [ ! -z "$EXTRA_PIP_PACKAGES" ]; then
-  echo "+uv pip install $EXTRA_PIP_PACKAGES"
-  uv pip install --system $EXTRA_PIP_PACKAGES
-fi
-
-if [ -z "$*" ]; then
-  echo "\
-  ___ ___ ___ ___ ___ ___ _____ 
- | _ \ _ \ __| __| __/ __|_   _|
- |  _/   / _|| _|| _| (__  | |  
- |_| |_|_\___|_| |___\___| |_|  
-
-"
-  exec bash --login
+# If a command is provided, run it; otherwise start Prefect
+if [ "$#" -eq 0 ]; then
+    exec prefect server start
 else
-  exec "$@"
+    exec "$@"
 fi
